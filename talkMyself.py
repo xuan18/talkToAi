e3e8a974f49c38c9bcefbc3dbd1312d704a28b79
嘉轩xuan18 <ljxzsx4@gmail.com>
2024-12-24 15:48:47 +0800

    add step
    增加问题：此问题属于个人，还是超越个人？

--- talkMyself.py
+++ talkMyself.py
@@ -40,11 +40,11 @@ while loop_step:
             first_talk = False
 
             # 开始对话
-            print(f"# 明义优化文本 \n user: {pyperclip.paste()}\n")
+            print(f"\n\n{pyperclip.paste()}\n")
             history.append(pyperclip.paste())
             write_history(f"# 明义优化文本 \n user: {pyperclip.paste()}\n")
-            print(f"# 明义优化文本 \n ")
-            text = input("What?\n")
+            #print(f"# 明义优化文本 \n ")
+            text = input("\nWhat?\n")
             write_history(text)
             history.append(text)
 
@@ -52,8 +52,8 @@ while loop_step:
         # 询问用户：‘为何提出此？’
         if step_two == True:
             step_two = False
-            print("\n# 寻找意图")
-            ask_intent = input(f"assistant: 为何提出【{text}】\n")
+            #print("\n# 寻找意图")
+            ask_intent = input(f"\nassistant: 为何提出【{text}】\n")
             if ask_intent == '返回':
                 step_one = True
                 step_two = True
@@ -61,40 +61,49 @@ while loop_step:
             write_history(ask_intent)
             pyperclip.copy(ask_intent)
 
-        # 步骤三（简称：明义优化回答）
-        # 针对用户上述回答中可能存在的语义不明确之处，持续提问以获得更清晰的理解。然后，基于这一理解优化并重写回答。以 Markdown 代码输出：优化后的回答。
-
-        # 步骤四（简称：询问问题）
-        # 询问以下方括号内的问题，注意每次仅提出一个问题，且不添加额外的文字：
-        # [
-        # 最终目标是什么？
-        # 目标是倾向于进取还是保守？
-        # 决策场景是什么？
-        # ]
-        print("\n# 询问问题")
+        # 步骤四（此待办属于个人，还是超越个人？）
+        #print("\n# 询问问题")
         if step_four == True:
-            temporary = input("目标倾向和最终目标是什么？\n")
+            temporary = input("\n此待办属于个人，还是超越个人？\n") #依据：https://workflowy.com/#/fc4dd028269b
             step_four = False
             if temporary == '返回':
                 step_two = True
                 step_four = True
                 break
 
-            #保存上一次的最终目标
+            #保存上一次该问题的答案
+            if save_target == False:
+                ultimate_goal = temporary
+                # print(ultimate_goal)
+                save_target = True
+            #开启下一步
+            step_five = True
+
+        # 步骤五（询问：目标倾向和最终目标是什么？）
+        if step_five == True:
+            temporary = input("\n目标倾向和最终目标是什么？\n")
+            step_five = False
+            if temporary == '返回':
+                step_four = True
+                step_five = True
+                break
+
+            #保存上一次该问题的答案
             if save_target == False:
                 ultimate_goal = temporary
                 # print(ultimate_goal)
                 save_target = True
 
-            # 步骤五（简称：重申）
-            # 以 Markdown 代码输出：步骤三中优化后的回答。
+            #开启下一步
             step_six = True
 
         # 步骤六（询问：决策场景是什么？）
         if step_six == True:
             step_six = False
 
-            if temporary == "最后一步":
+            if temporary == "返回":
+                step_five = True
+                step_six = True
                 break
             else:
                 ultimate_goal = temporary
